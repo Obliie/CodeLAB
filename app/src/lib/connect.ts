02fb91aca415b24416ba16a39b712bef9c7b22de
Obliie <kiirohotaru@protonmail.com>
2024-02-27 01:06:22 +0000

    fix: frontend transport host

--- app/src/lib/connect.ts
+++ app/src/lib/connect.ts
@@ -3,11 +3,6 @@ import { createPromiseClient, PromiseClient } from '@connectrpc/connect';
 import { createGrpcWebTransport } from '@connectrpc/connect-web';
 import { useMemo } from 'react';
 
-// TODO Use URLs once TLS issue is resolved.
-export const transport = createGrpcWebTransport({
-    baseUrl: 'https://127.0.0.1:443/',
-});
-
 export const serverTransport = createGrpcWebTransport({
     baseUrl: 'https://api-gateway:443/',
 });
@@ -16,6 +11,11 @@ export const serverTransport = createGrpcWebTransport({
  * Get a promise client for the given service.
  */
 export function useClient<T extends ServiceType>(service: T): PromiseClient<T> {
+    // TODO The transport should only be created once like the server transport
+    const transport = createGrpcWebTransport({
+        baseUrl: `https://${window.location.host}:443/`,
+    });
+
     // We memoize the client, so that we only create one instance per service.
     return useMemo(() => createPromiseClient(service, transport), [service]);
 }
