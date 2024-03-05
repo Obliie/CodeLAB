3680f44b080412b1691587686f31e732a58bcabd
Obliie <kiirohotaru@protonmail.com>
2024-03-05 01:32:15 +0000

    feat: allow users to filter problems by name

--- app/src/components/ProblemSummaryAccordion.tsx
+++ app/src/components/ProblemSummaryAccordion.tsx
@@ -9,21 +9,35 @@ import ProblemActions from "./ProblemActions";
 import { DeleteProblemRequest } from "@/actions/DeleteProblemRequest";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import { useSession } from "next-auth/react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
 
 export default function ProblemSummaryAccordion({ problemSummaries, deleteProblem, nav }: { deleteProblem: Function, problemSummaries: ProblemSummary[], nav: string }) {
     const { data: session } = useSession();
+    const [filteredSummaries, setFilteredSummaries] = useState(problemSummaries)
 
-    return (problemSummaries.map(problem => (
-        <Accordion key={problem.id}>
-            <AccordionSummary expandIcon={<ExpandMore />}>
-                <Typography>{problem.title}</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-                <Typography>{problem.summary}</Typography>
-            </AccordionDetails>
-            <AccordionActions sx={{marginBottom: '10px', gap: '5px', marginRight: '10px'}}>
-                <ProblemActions problemId={problem.id} deleteAction={deleteProblem} nav={nav} isOwner={session ? (session?.user.email === problem.owner) : false}/>
-            </AccordionActions>
-        </Accordion>
-    )));
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log("val", event.target.value);
+
+        setFilteredSummaries([...problemSummaries.filter(summary => summary.title.toLowerCase().includes(event.target.value.toLowerCase()))])
+    }
+
+    return (
+    <Box>
+        <TextField fullWidth id="problem-filter" label="Search..." variant="outlined" size="small" onChange={handleChange} sx={{ paddingBottom: '10px' }} />
+        {filteredSummaries.map(problem => (
+            <Accordion key={problem.id}>
+                <AccordionSummary expandIcon={<ExpandMore />}>
+                    <Typography>{problem.title}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>{problem.summary}</Typography>
+                </AccordionDetails>
+                <AccordionActions sx={{marginBottom: '10px', gap: '5px', marginRight: '10px'}}>
+                    <ProblemActions problemId={problem.id} deleteAction={deleteProblem} nav={nav} isOwner={session ? (session?.user.email === problem.owner) : false}/>
+                </AccordionActions>
+            </Accordion>
+        ))}</Box>
+    );
 }
