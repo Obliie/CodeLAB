8f545eb11a1a4b308b6b838ca3f7924f16f31ece
mohsin19010 <12mkhan01@gmail.com>
2024-05-08 22:44:23 +0100

    mongo connection

--- services/problemgo/src/main/main.go
+++ services/problemgo/src/main/main.go
@@ -3,14 +3,76 @@ package main
 import (
 	"log"
 	"net"
+	"fmt"
+	"io/ioutil"
+	"context"
+
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
     "google.golang.org/grpc"
     "codelab/pkg/problem"
     "codelab/protobufs/services/v1"
-
 )
 
+var (
+    DATABASE_HOST string  = "problem-db"
+    DATABASE_PORT int     = 27001
+    DATABASE_NAME string  = "problem"
+)
+
+func readFile(path string) (string, error) {
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return string(content), nil
+}
+
 func main() {
+	database_username_file, err := readFile("../run/secrets/problemdb-root-username")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	database_password_file, err := readFile("../run/secrets/problemdb-root-password")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	MONGO_URI := fmt.Sprintf("mongodb://%s:%s@%s:%d/%s?authSource=admin",
+	database_username_file, database_password_file, DATABASE_HOST, DATABASE_PORT, DATABASE_NAME)
+
+    clientOptions := options.Client().ApplyURI(MONGO_URI)
+    client, err := mongo.Connect(context.Background(), clientOptions)
+	
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	// Check the connection.
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+	log.Fatal(err)
+	}
+
+	// Create collection
+	client.Database("testdb").Collection("test")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to db")
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 19990))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+
 	// Create new gRPC server instance
 	s := problem.Server{}
 	grpcServer := grpc.NewServer()
@@ -18,4 +80,8 @@ func main() {
 
 	// Register gRPC server
 	v1.RegisterProblemServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
 }
--- services/problemgo/src/main/pkg/problem/problem.go
+++ services/problemgo/src/main/pkg/problem/problem.go
@@ -1,15 +1,132 @@
 package problem
 
 import (
-	"log"
-	"context"
-	"codelab/protobufs/services/v1"
+    "log"
+    "context"
+    services "codelab/protobufs/services/v1"
+    common "codelab/protobufs/common/v1"
 )
 
 type Server struct {
+	services.UnimplementedProblemServiceServer
 }
 
-func (s *Server) AddTestData(ctx context.Context, req *v1.AddTestDataRequest) (*v1.AddTestDataResponse, error) {
+func (s *Server) AddTestData(ctx context.Context, req *services.AddTestDataRequest) (*services.AddTestDataResponse, error) {
 	log.Printf("Received Title from client: %s", req.ProblemId) // Corrected field name from req.id to req.ProblemId
-	return &v1.AddTestDataResponse{Success: true}, nil // Corrected field name to Success
+	return &services.AddTestDataResponse{Success: true}, nil // Corrected field name to Success
+}
+
+func (s *Server) RemoveTestData(ctx context.Context, req *services.RemoveTestDataRequest) (*services.RemoveTestDataResponse, error) {
+	log.Printf("Received Title from client: %s", req.ProblemId) // Corrected field name from req.id to req.ProblemId
+	return &services.RemoveTestDataResponse{Success: true}, nil // Corrected field name to Success
+}
+
+func (s *Server) UpdateTestData(ctx context.Context, req *services.UpdateTestDataRequest) (*services.UpdateTestDataResponse, error) {
+	log.Printf("Received Title from client: %s", req.Test.Id) // Corrected field name from req.id to req.ProblemId
+	return &services.UpdateTestDataResponse{Success: true}, nil // Corrected field name to Success
+}
+
+func (s *Server) CreateProblem(ctx context.Context, req *services.CreateProblemRequest) (*services.CreateProblemResponse, error) {
+	log.Printf("Received Title from client: %s", req.Problem.Id) 
+	return &services.CreateProblemResponse{Success: true}, nil // 
+}
+
+func (s *Server) UpdateProblem(ctx context.Context, req *services.UpdateProblemRequest) (*services.UpdateProblemResponse, error) {
+    log.Printf("Received Title from client: %s", req.Problem.Id) 
+    return &services.UpdateProblemResponse{Success: true}, nil // 
+}
+
+func (s *Server) CreateProblemGroup(ctx context.Context, req *services.CreateProblemGroupRequest) (*services.CreateProblemGroupResponse, error) {
+	log.Printf("Received Title from client: %s", req.Group.Id) 
+	return &services.CreateProblemGroupResponse{Success: true}, nil // 
+}
+
+func (s *Server) UpdateProblemGroup(ctx context.Context, req *services.UpdateProblemGroupRequest) (*services.UpdateProblemGroupResponse, error) {
+    log.Printf("Received Title from client: %s", req.Group.Id) 
+    return &services.UpdateProblemGroupResponse{Success: true}, nil // 
+}
+
+func (s *Server) DeleteProblem(ctx context.Context, req *services.DeleteProblemRequest) (*services.DeleteProblemResponse, error) {
+	log.Printf("Received Title from client: %s", req.ProblemId) 
+	return &services.DeleteProblemResponse{Success: true}, nil // 
+}
+
+func (s *Server) DeleteProblemGroup(ctx context.Context, req *services.DeleteProblemGroupRequest) (*services.DeleteProblemGroupResponse, error) {
+	log.Printf("Received Title from client: %s", req.GroupId) 
+	return &services.DeleteProblemGroupResponse{Success: true}, nil // 
+}
+
+
+func (s *Server) GetProblem(ctx context.Context, req *services.GetProblemRequest) (*services.GetProblemResponse, error) {
+    log.Printf("Received Title from client: %s", req.ProblemId) 
+
+    // Initialize a Problem struct
+    p := common.Problem{
+        Id: "42", 
+    }
+
+    response := &services.GetProblemResponse{
+        Problem: &p,
+    }
+
+    return response, nil
+}
+
+func (s *Server) GetProblemGroup(ctx context.Context, req *services.GetProblemGroupRequest) (*services.GetProblemGroupResponse, error) {
+    log.Printf("Received Title from client: %s", req.GroupId) 
+
+    // Initialize a Problem struct
+    p := common.ProblemGroup{
+        Id: "42", 
+    }
+
+    response := &services.GetProblemGroupResponse{
+        Group: &p,
+    }
+
+    return response, nil
+}
+
+func (s *Server) GetProblemGroupList(ctx context.Context, req *services.GetProblemGroupListRequest) (*services.GetProblemGroupListResponse, error) {
+    log.Printf("Received Title from client: %s", req.Limit) 
+
+    // Initialize a Problem struct
+    p1 := common.ProblemGroup{
+        Id: "42", 
+    }
+
+	p2 := common.ProblemGroup{
+        Id: "41", 
+    }
+
+	// Put them in a slice
+	problemGroups := []*common.ProblemGroup{&p1, &p2}
+
+    response := &services.GetProblemGroupListResponse{
+        Groups: problemGroups,
+    }
+	
+    return response, nil
+}
+
+func (s *Server) GetProblemSummaries(ctx context.Context, req *services.GetProblemSummariesRequest) (*services.GetProblemSummariesResponse, error) {
+    log.Printf("Received Title from client: %s", req.Limit) 
+
+    // Initialize a Problem struct
+    p1 := common.ProblemSummary{
+        Id: "42", 
+    }
+
+	p2 := common.ProblemSummary{
+        Id: "41", 
+    }
+
+	// Put them in a slice
+	GetProblemSummaries := []*common.ProblemSummary{&p1, &p2}
+
+    response := &services.GetProblemSummariesResponse{
+        ProblemSummaries: GetProblemSummaries,
+    }
+	
+    return response, nil
 }
